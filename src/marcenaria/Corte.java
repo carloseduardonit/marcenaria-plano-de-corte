978cb7f240d1aef1f1a0637a836fb9fccc86f410
Carlos Eduardo dos Santos Figueiredo <42427944+carloseduardonit@users.noreply.github.com>
2019-05-24 21:01:07 -0300

    24/05/19 projeto marcenaria

--- src/marcenaria/Corte.java
+++ src/marcenaria/Corte.java
@@ -6,6 +6,7 @@
 package marcenaria;
 
 import java.sql.*;
+import javax.swing.JOptionPane;
 import marcenaria.dado.ModuloConector;
 
 /**
@@ -14,6 +15,11 @@ import marcenaria.dado.ModuloConector;
  */
 public class Corte {
 
+    public static void main(String[] args) {
+        PecaParaCortar(220, 160, 220,
+                 160, true);
+        dividirCorte(0, 0, 0, 0, 0);
+    }
     static Connection conexao;
     static PreparedStatement pst;
     static Statement stmt;
@@ -64,11 +70,42 @@ public class Corte {
      */
     public static void PecaParaCortar(double comprChapa, double largChapa, double compPeca, double largPeca, boolean rot) {
         for (int i = 0; i < getCompriCorte().length; i++) {
-            if (largChapa < largPeca && comprChapa > largPeca && comprChapa > largPeca && rot == true) {
-                //
+            if (largChapa < largPeca && comprChapa > largPeca && largChapa > compPeca && rot == true) {
+                //se a largura da chapa for menor que a largura da peça E
+                //se o comprimento da chapa for maior que a largura da peça E
+                //se a largura da chapa for maior que o comprimento da peca E
+                // se rot for true
                 setCompriCorte(i, comprChapa, largPeca);
                 setLargCorte(i, largChapa, compPeca);
-            } 
+            } else if (largChapa < largPeca && comprChapa > largPeca && largChapa > compPeca && rot == false) {
+                //se a largura da chapa for menor que a largura da peça E
+                //se o comprimento da chapa for maior que a largura da peça E
+                //se a largura da chapa for maior que o comprimento da peca E
+                // se rot for false
+                String frase = "";
+                if (largChapa < largPeca) {
+                    frase = "A Medida da Largura da peca: " + largPeca
+                            + "\ne Maior a Medida da Largura da Chapa: " + largChapa;
+                } else {
+                    frase = "A Medida da Largura da peca: " + largPeca
+                            + "\ne Menor ou Iqual a Medida da Largura da Chapa: " + largChapa;
+                }
+                if (comprChapa < compPeca) {
+                    frase += "\nA Medida da Comprimento da peca: " + compPeca
+                            + "\ne Maior a Medida da Comprimento da Chapa: " + comprChapa;
+                } else {
+                    frase += "\nA Medida da Comprimento da peca: " + compPeca
+                            + "\ne Menor ou Iqual a Medida da Comprimento da Chapa: " + comprChapa;
+                }
+                JOptionPane.showMessageDialog(null, frase);
+            } else if(comprChapa==compPeca&&largChapa==largPeca){
+                
+            }else {
+                setCompriCorte(i, comprChapa, compPeca);
+                setLargCorte(i, largChapa, largPeca);
+                String a = " c: " + getCompriCorte(i) + " l: " + getLargCorte(i);
+                JOptionPane.showMessageDialog(null, a);
+            }
         }
     }
 
@@ -124,6 +161,14 @@ public class Corte {
     }
 
     /**
+     * @param pos
+     * @return
+     */
+    public static double getLargCorte(int pos) {
+        return largCorte[pos];
+    }
+
+    /**
      * @param largCorte
      */
     public static void setLargCorte(double[] largCorte) {
@@ -144,11 +189,42 @@ public class Corte {
      * @param largPeca
      */
     public static void setLargCorte(int pos, double largChapa, double largPeca) {
-        if (largPeca > largChapa || largPeca < 0 || largChapa < 0) {
+        if (largPeca > largChapa || largPeca <= 0 || largChapa <= 0) {
             Corte.largCorte[pos] = -1;
         } else {
             Corte.largCorte[pos] = largChapa - largPeca;
         }
 
+    }
+
+    /**
+     * @param pos
+     * @param largChapa
+     * @param largPeca
+     * @param comprChapa
+     */
+    public static void horizontalCorte(int pos, double largChapa, double largPeca, double comprChapa) {
+        setLargCorte(pos, largChapa, largPeca);
+        setCompriCorte(pos, comprChapa);
+    }
+
+    /**
+     * @param pos
+     * @param comprChapa
+     * @param comprPeca
+     * @param largChapa
+     */
+    public static void verticalCorte(int pos, double comprChapa, double comprPeca, double largChapa) {
+        setCompriCorte(pos, comprChapa, comprPeca);
+        setLargCorte(pos, largChapa);
+    }
+    public static void dividirCorte(int pos, double comprChapa, double comprPeca, double largChapa,double largPeca){
+        for(int i=0;i<=1;i++){
+            if(i==0){
+                verticalCorte(pos, comprChapa, comprPeca, largChapa);
+            }else{
+                horizontalCorte(pos, largChapa, largPeca, comprChapa);
+            }
+        }
     }
 }

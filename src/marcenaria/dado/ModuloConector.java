b35b3caec0a1c2f4fd205f923842e85e8a926fdf
Carlos Eduardo dos Santos Figueiredo <42427944+carloseduardonit@users.noreply.github.com>
2019-07-11 22:58:17 -0300

    11/07/19 projeto marcenaria 1 depois das modificaçoes do dia: 1-atualizaçao da Javadoc  da classe  ModuloConector, 2-Criação e alteração de metodo QuantLinha e QuantColuna.

--- src/marcenaria/dado/ModuloConector.java
+++ src/marcenaria/dado/ModuloConector.java
@@ -13,30 +13,70 @@ import marcenaria.Const.Messagem;
  * @version 1.2
  * @author Carlos Eduardo dos Santos Figueiredo
  * @see ModuloConector
+ * @since 01/05/2019
  */
 public class ModuloConector {
 
+    /**
+     * Nome do banco de dados
+     */
     public static final String DATABASE = "teste";
+    /**
+     * Driver do banco de dados
+     */
     public static final String DRIVER = "com.mysql.jdbc.Driver";
+    /**
+     * A URL do banco de dados
+     */
     public static final String URL = "jdbc:mysql://localhost:3306/";
+    /**
+     * A URL + Nome do banco de dados
+     */
     public static final String URLD = URL + DATABASE;
+    /**
+     * Usuario do banco de dados
+     */
     public static final String USER = "root";
+    /**
+     * Senha do banco de dados
+     */
     public static final String PASS = "";
+    //Variaveis de gerencimento do bano de dados
+    /**
+     * 
+     */
     private static Connection conexao = null;
+    /**
+     * 
+     */
     private static PreparedStatement pst = null;
+    /**
+     * 
+     */
     private static ResultSet rs = null;
+    /**
+     * 
+     */
+    private static ResultSetMetaData rsmd = null;
+    /**
+     * 
+     */
     private static Statement stmt = null;
 
+    /**
+     * Este metodo faz a conexão com o banco de dados
+     */
     private static void conector() {
         conexao = getConecction();
 
     }
 
     /**
-     * @version 1.0
-     * Este metodo faz a conexao com o banco de dados MYSQL utilizado as variaveis
-     * finais
+     * Este metodo faz a conexao com o banco de dados MYSQL utilizado as
+     * variaveis finais
      *
+     * @version 1.0
+     * @since 01/05/2019
      * @return a conexao conexao com o banco de dado
      */
     public static java.sql.Connection getConecction() {
@@ -77,9 +117,8 @@ public class ModuloConector {
     }
 
     /**
-     * @version 1.0
-     * Este Metodo faz o fechamento da conexao
-     *
+     * @version 1.0 Este Metodo faz o fechamento da conexao
+     * @since 01/05/2019
      * @param con - Fecha a conexao do banco
      */
     public static void fecharConexao(Connection con) {
@@ -93,11 +132,12 @@ public class ModuloConector {
     }
 
     /**
-     * 
+     *
      * Este Metodo faz o fechamento da conexao e Resultado
      *
+     * @since 01/05/2019
      * @param con - Fecha a conexao do banco
-     * @param rs  - Fecha o resultado do Banco
+     * @param rs - Fecha o resultado do Banco
      */
     public static void fecharConexao(Connection con, ResultSet rs) {
         try {
@@ -115,8 +155,9 @@ public class ModuloConector {
     /**
      * Este Metodo faz o fechamento da conexao, Resultado e Editação
      *
+     * @since 01/05/2019
      * @param con - Fecha a conexao do banco
-     * @param rs  - Fecha o resultado do Banco
+     * @param rs - Fecha o resultado do Banco
      * @param pst -Fecha a
      */
     public static void fecharConexao(Connection con, ResultSet rs, PreparedStatement pst) {
@@ -135,9 +176,10 @@ public class ModuloConector {
     /**
      * Este Metodo faz o fechamento da conexao, Resultado e Editação
      *
-     * @param con  Fecha a conexao do banco
-     * @param rs   Fecha o resultado do Banco
-     * @param pst  Fecha a
+     * @since 01/05/2019
+     * @param con Fecha a conexao do banco
+     * @param rs Fecha o resultado do Banco
+     * @param pst Fecha a
      * @param stmt Fecha a
      */
     public static void fecharConexao(Connection con, ResultSet rs, PreparedStatement pst, Statement stmt) {
@@ -155,6 +197,8 @@ public class ModuloConector {
 
     /**
      * Fazer
+     *
+     * @since 01/05/2019
      */
     public static void criarBackupdataBase() {
         Connection conexao = getConecction();
@@ -171,10 +215,13 @@ public class ModuloConector {
     }
 
     /**
-     * Este Metodo Verifica se determinada Tabela nao existe return um valor Boolean
+     * Este Metodo Verifica se determinada Tabela nao existe return um valor
+     * Boolean
      *
+     * @since 01/05/2019
      * @param Tabela Setar uma Informação de tipo String com o nome da Tabela
      * @return Retornar um valor Boolean
+     *
      */
     public static Boolean VerificarNaoExistirTabela(String Tabela) {
         try {
@@ -197,18 +244,96 @@ public class ModuloConector {
     }
 
     /**
-     * Este Metodo Obtem o numero de Coluna no banco
+     * Este Metodo Obtem o numero de Coluna de uma determinada tabela no banco.
+     *
+     * @since 10/07/2019
+     * @version 1.4
+     * @param tabela Setar uma informação devalor String com o none da tabela .
+     * @return Retornar uma infornação de valor inteiro com a quantidade de
+     * coluna.
      */
-    public static int quantColuna(String a) {
+    public static int quantColuna(String tabela) {
+        String sql = "selec * from ?";
+        return quantColuna(tabela, sql);
+    }
+
+    /**
+     * Este Metodo Obtem o numero de Coluna de uma determinada tabela no banco.
+     *
+     * @since 11/07/2019
+     * @version 1.5
+     * @param tabela Setar uma informação devalor String com o none da tabela .
+     * @param sql Setar uma Informação de Valor String com a instrução MYSQL.
+     * @return Retornar uma informação de valor inteiro com a quantidade de
+     * coluna.
+     */
+    public static int quantColuna(String tabela, String sql) {
+        conector();
+        try {
+            if (VerificarNaoExistirTabela(tabela)) {
+                pst = conexao.prepareStatement(sql);
+                pst.setString(1, tabela);
+                rs = pst.executeQuery();
+                if (rs.next()) {
+                    rsmd = rs.getMetaData();
+                    return rsmd.getColumnCount();
+                }else{
+
+                }
+            }else   {
+                
+            }
+        } catch (Exception e) {
+            Messagem.chamarTela("Metodo QuantColuna da Classe ModuloConector: " + e);
+        }
 
         return 0;
     }
 
     /**
-     * Este Metodo Obtem o numero de Coluna no banco
+     * Este Metodo Obtem o numero de Linha de uma determinada tabela no banco
+     *
+     * @since 10/07/2019
+     * @param tabela Setar uma informação devalor String com o none da tabela .
+     * @return Retornar uma informação de valor inteiro com a quantidade de
+     * linha.
      */
-    public static int quantLinha(String a) {
+    public static int quantLinha(String tabela) {
+        String sql = "selec * from " + tabela;
+        return quantLinha(tabela, sql);
+    }
 
+    /**
+     * Este Metodo Obtem o numero de Linha de uma determinada tabela no banco
+     *
+     * @since 11/07/2019
+     * @version 1.5
+     * @param tabela Setar uma informação devalor String com o none da tabela .
+     * @param sql Setar uma Informação de Valor String com a instrução MYSQL.
+     * @return Retornar uma informação de valor inteiro com a quantidade de
+     * linha.
+     */
+    // para obter o numero de linha eu tenho que sabe se  Tabela existe; Depois saber ser a instrução mysql obter algum resultado
+    public static int quantLinha(String tabela, String sql) {
+        try {
+            conector();
+            if (VerificarNaoExistirTabela(tabela)) {
+                pst = conexao.prepareStatement(sql);
+                pst.setString(1, tabela);
+                rs = pst.executeQuery();
+                stmt = rs.getStatement();
+                if (rs.next()) {
+                    return stmt.getMaxRows();
+                }else{
+                    Messagem.chamarTela("");
+                }
+            } else {
+                Messagem.chamarTela("");
+            }
+        } catch (Exception e) {
+            Messagem.chamarTela("Metodo QuantLinha da Classe ModuloConector: " + e);
+        }
         return 0;
     }
+
 }
